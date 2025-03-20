6122882b2f85f54da6a25b8a837058d8b48e285a
Alexander Flick <xela.fli@gmail.com>
2025-03-20 21:12:01 +0000

    add staging and fact tables.

---
+++ dbt/models/marts/fct_rewe_products.sql
@@ -0,0 +1,30 @@
+with rewe_products as (
+    select * from {{ ref('stg_rewe_products') }}
+),
+
+rewe_articles as(
+    select * from {{ ref('stg_rewe_products_articles') }}
+),
+
+final as (
+
+    select
+        DISTINCT(rewe_products.product_id),
+        rewe_products.product_name,
+        rewe_products.category_path,
+        rewe_products.link,
+        rewe_products.brand,
+        rewe_articles.listing_price,
+        rewe_articles.listing_regular_price,
+        rewe_articles.listing_discount_rate,
+        rewe_articles.listing_discount_valid_to,
+        rewe_articles.listing_grammage
+    from rewe_products
+    join rewe_articles
+    on rewe_products._dlt_id=rewe_articles._dlt_parent_id
+    where rewe_products.category_path not like "Tierbedarf%" and
+    rewe_products.category_path not like "Küche & Haushalt%" and
+    rewe_products.category_path not like "Drogerie & Kosmetik%"
+)
+
+select * from final
